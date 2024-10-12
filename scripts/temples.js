20d7639de3b5fae1f4e6e5fa43158a1c1c1ea0e5
Wutieyin <136192696+Wutieyin@users.noreply.github.com>
2024-10-12 06:36:22 +0100

    Current year added

--- scripts/temples.js
+++ scripts/temples.js
@@ -9,3 +9,7 @@ hamButton.addEventListener('click', () => {
 
 let text = document.lastModified;
 document.getElementById("demo").innerHTML = text;
+
+var date = new Date(); 
+var year = date.getFullYear();
+document.getElementById('curYr').innerHTML = year
